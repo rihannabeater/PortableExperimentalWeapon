03cc4c0973febe2e2a7f69f4e85e1a5df7130889
Will <will@william-d-taylor.com>
2019-04-30 22:35:40 -0500

    Added webcam to shell script

--- Keyboard/key_pew.sh
+++ Keyboard/key_pew.sh
@@ -1,4 +1,6 @@
 nc -l 2222 | mplayer -fps 200 -demuxer h264es - &
-ssh -t pi@10.10.10.10 'raspivid -t 0 -rot 270 -w 500 -h 500 -hf -fps 20 -o - | nc 10.10.10.5 2222' &
+nc -l 2223 | mplayer -fps 30 -demuxer h264es - &
+ssh -t pi@10.10.10.10 'raspivid -t 0 -rot 270 -w 500 -h 500 -hf -fps 20 -o - | nc 10.10.10.1 2222' &
+ssh -t pi@10.10.10.10 'ffmpeg -i /dev/video0 -f h264 -preset ultrafast pipe:1 | nc 192.168.0.118 2223' &
 ssh -Y pi@10.10.10.10. 'cd Desktop/ && python key.py' 
 
