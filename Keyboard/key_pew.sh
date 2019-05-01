4697a50a62479aa40f4f2a36f48a4071508f617d
Will <will@william-d-taylor.com>
2019-05-01 11:59:18 -0500

    Hell yeah we done bois

--- Keyboard/key_pew.sh
+++ Keyboard/key_pew.sh
@@ -1,3 +1,4 @@
+# Must Set your ip to 10.10.10.1 and have mplayer installed
 nc -l 2222 | mplayer -fps 200 -demuxer h264es - &
 nc -l 3333 | mplayer -fps 30 -demuxer h264es - &
 ssh -t pi@10.10.10.10 'raspivid -t 0 -rot 270 -w 500 -h 500 -hf -fps 20 -o - | nc 10.10.10.1 2222' &
---
+++ Keyboard/run.sh
@@ -0,0 +1,4 @@
+#!/bin/uxterm
+# may need to change the terminal
+
+ssh -Y pi@10.10.10.10. 'cd Desktop/ && python key.py' 
